94c7d79975d9d87f06a532d2806a08819740696a
CaptainLazarus <captainlazarus@pop-os.localdomain>
2021-08-25 01:06:41 +0530

    Works

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
+from time import sleep
+from urllib.request import urlopen
+from funcs import *
 
 # Read URL + checkpoint from file
 f = open("url.txt", "r")
 c = open("checkpoint.txt", "r")
 URL = f.readline().strip()
-CHECKPOINT = c.readline().strip()
+CHECKPOINT = int(c.readline().strip())
 f.close()
 c.close()
 
-#Complete URL
-FullURL = URL + CHECKPOINT
-print(FullURL)
+data = []
+checkpoint = []
+CHECKPOINT , PAGE = skip(URL , CHECKPOINT)
+# print(CHECKPOINT)
 
-PAGE = urlopen(URL)
+while True:
+  # sleep(0.05)
+  try:
+      print(CHECKPOINT)
+      FullURL = URL + str(CHECKPOINT)
+      # print(FullURL)
+      PAGE = urlopen(FullURL)
+      soup = BeautifulSoup(PAGE, "html.parser")
+      td = soup.find_all("td")
+      td = [i.text.strip().replace("," , ":") for i in td]
+      data.append(td)
+      checkpoint.append(CHECKPOINT)
+      CHECKPOINT += 1
+  except KeyboardInterrupt:
+        print("Shutdown requested...exiting")
+        break
+  except Exception as e:
+      print(e)
+      CHECKPOINT += 1
 
-# soup = BeautifulSoup(PAGE, "html.parser")
-# h4 = soup.find_all("h4")
-# h4 = [i.text for i in h4]
-# print(h4)
-#
-# f = open('2.csv', 'w')
-# writer = csv.writer(f)
-# for i in h4:
-#     writer.writerow([i])
+c = open("checkpoint.txt", "w")
+c.write(str(CHECKPOINT))
+
+f = open('1.csv', 'a')
+writer = csv.writer(f)
+for i in data:
+    writer.writerow(i)
